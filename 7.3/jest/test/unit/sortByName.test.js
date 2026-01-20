710b2006443ea6a751c239082c75a3d63f9c763f
Anastasia <nastya.il03@mail.ru>
2026-01-20 23:12:07 +0400

    Update package.json and tests

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,23 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+
+  test("With dublicate names should return 0", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Гарри Поттер",
+    ];
+
+    const expected = [
+      "Властелин Колец",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+
+  });
 });
